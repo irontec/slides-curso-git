4a698917477772caf6415016118e30413a9b0827
Victor Saldarriaga <vsaldalau@gmail.com>
2019-05-10 12:45:48 +0200

    Updates

---
+++ precache-manifest.8a8ef25d3355ccec092ad01f89148338.js
@@ -0,0 +1,208 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf83e10e32a27196b01baed8f267bb43",
+    "url": "media/commit_internals_5.png"
+  },
+  {
+    "revision": "27ddf47c7a7de1b45858d6d59a4017f5",
+    "url": "notes.html"
+  },
+  {
+    "revision": "e16be04a93af4a1888f99462c946a788",
+    "url": "media/git_areas.png"
+  },
+  {
+    "revision": "a755a8c13f30c132f8e4",
+    "url": "hljs-langs/typescript.js"
+  },
+  {
+    "revision": "95f0e9807ccd6eab8cc41550c400511d",
+    "url": "index.html"
+  },
+  {
+    "revision": "48ac21230fcb4ecb1a71e20b1b8ba85f",
+    "url": "media/tracking_ficheros_ciclo_vida.png"
+  },
+  {
+    "revision": "0a93f293768c5ea3fd3728ed8bae3aa8",
+    "url": "media/testing_repo_state_4.png"
+  },
+  {
+    "revision": "49439579e85795956803de33f805c68b",
+    "url": "media/testing_repo_state_3.png"
+  },
+  {
+    "revision": "05453a3d7589120784e8ce23c4919bbf",
+    "url": "media/testing_repo_state_2.png"
+  },
+  {
+    "revision": "aa62a2869d0991698e2db7ad43139a3d",
+    "url": "media/testing_repo_state_1.png"
+  },
+  {
+    "revision": "9a9385f2ed37b837c76895b69a49ac71",
+    "url": "media/rebasing_ejm_4.png"
+  },
+  {
+    "revision": "e74052316b9e02b30b852fd7e4b7c3eb",
+    "url": "media/rebasing_ejm_3.png"
+  },
+  {
+    "revision": "997dc13cb99f9015e9474b0d2cecdd69",
+    "url": "media/rebasing_ejm_2.png"
+  },
+  {
+    "revision": "68058c5dfda7f7670f1fad895a41d245",
+    "url": "media/rebasing_ejm_1.png"
+  },
+  {
+    "revision": "3db29d49fd773ed4ae35c27e63088dd1",
+    "url": "media/gitlab-project-new.png"
+  },
+  {
+    "revision": "c5e74152a707a5ba2ef642c9aca2b904",
+    "url": "media/logo_irontec.png"
+  },
+  {
+    "revision": "c86ac01595d6a7010905c340c1142ec1",
+    "url": "media/gitlab-group-new-group.png"
+  },
+  {
+    "revision": "3b5a5b21f7f4f2a614981c224380e9a8",
+    "url": "media/gitlab-options-menu.png"
+  },
+  {
+    "revision": "4b0bd7fd78ea42c9a048b3e15d03da87",
+    "url": "media/gitlab-dashboard-users.png"
+  },
+  {
+    "revision": "e279925af48b5fbf0be958b186740c8c",
+    "url": "media/git-flow-5-hotfix.png"
+  },
+  {
+    "revision": "2b9aa77b38a6c9fde12d0db15bdd0894",
+    "url": "media/git-flow-5-hotfix-master-dev.png"
+  },
+  {
+    "revision": "d8e061351a5b8e532d87e7b3cb2ebfa6",
+    "url": "media/fondo_presentacion.jpg"
+  },
+  {
+    "revision": "23e46c9fe9de238ef06e6d508f778979",
+    "url": "media/centralized_workflow.png"
+  },
+  {
+    "revision": "d4ad935e54b632584d6dc704e95cd90f",
+    "url": "media/commit_internals_1.png"
+  },
+  {
+    "revision": "28fc6dd6a0b137df071f79721d6e2574",
+    "url": "media/commit_internals_2.png"
+  },
+  {
+    "revision": "74df35dd64dd205bcdd295784ad5b031",
+    "url": "media/commit_internals_3.png"
+  },
+  {
+    "revision": "04952414967ce87eaf99297b065936db",
+    "url": "media/commit_internals_4.png"
+  },
+  {
+    "revision": "57b27cf00e6bb0cc26b3feca980b9357",
+    "url": "media/gitlab_logo.svg"
+  },
+  {
+    "revision": "bd1bca70f299f7eb530c6821e4776581",
+    "url": "media/gitlab_cicd_pipeline_infograph.png"
+  },
+  {
+    "revision": "9a0a2a4c5fc1aa1407e7c07ef14dc821",
+    "url": "media/commit_internals_6.png"
+  },
+  {
+    "revision": "d18cbd2ef03bebc08ae6dd6c931df696",
+    "url": "media/git_flow_1.png"
+  },
+  {
+    "revision": "4d2e6b220ae233ad0da28745d66b8309",
+    "url": "media/git-flow-2-feature.png"
+  },
+  {
+    "revision": "6c50ae26a461df3f57e4c7da9dcc33bb",
+    "url": "media/git-flow-3-release.png"
+  },
+  {
+    "revision": "1868810fd09b2f2e50882a3becac8bd5",
+    "url": "media/git-flow-4-release-master-dev.png"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "revision": "c19aca6bdb0fc9b27eea",
+    "url": "app.css"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "revision": "c19aca6bdb0fc9b27eea",
+    "url": "app.js"
+  },
+  {
+    "revision": "6e29ff6d4e03bd5ba8b3d5556174789e",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "revision": "d8e061351a5b8e532d87e7b3cb2ebfa6",
+    "url": "aef240019ba9ff99f13b6f27ca44e38b.jpg"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  },
+  {
+    "revision": "9d9be852aec6202f99a33cf9cc292d9c",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "revision": "8a63aaf795c65b9a001dfc6bf7387293",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "revision": "0f1d1c3121fc27d452d6378453c248fc",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  }
+];
